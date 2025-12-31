6177e296d959d0e27790797b675a22918f670bfc
Beast <11330075@dcsh.tp.edu.tw>
2025-12-31 22:06:08 +0800

    1006

--- default/urls.py
+++ default/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,reverse_lazy
 from . import views
 from  django.views.generic import RedirectView
 urlpatterns = [
-    path('poll/', views.PollList.as_view()),
+    path('poll/', views.PollList.as_view(),name='poll_list'),
     path('poll/<int:pk>/', views.PollDetail.as_view()),
     path('option/<int:pk>/', views.PollVote.as_view()),
     path('', RedirectView.as_view(url=reverse_lazy('poll_list')))
